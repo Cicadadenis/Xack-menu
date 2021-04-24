0a655654eb2fc71afb62f05550305c09a2e8235b
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-04-24 12:57:33 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ rm -rf /etc/profile
 cd
 git clone https://github.com/Cicadadenis/airgeddon
 cp -r /root/airgeddon/airgeddon.sh /bin/wifi-crack
-
+git clone https://github.com/Cicadadenis/ipfy
+cp -r /root/ipfy
 wget -P /etc https://raw.githubusercontent.com/Cicadadenis/Xack-menu/master/profile
 wget -P /etc https://raw.githubusercontent.com/Cicadadenis/Xack-menu/master/motd
 
