f6070f6ce5a329c8c25d361d4b571bcd3571b766
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-04-24 18:49:19 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@ rm -rf /etc/motd
 rm -rf /etc/profile
 cd
 git clone https://github.com/Cicadadenis/airgeddon
+chmod +x /root/airgeddon/airgeddon.sh
 cp -r /root/airgeddon/airgeddon.sh /bin/wifi-crack
+
 git clone https://github.com/Cicadadenis/ipfy
-chmod +x *
+chmod +x /root/ipfy/start-ipfy.sh
 cp -r /root/ipfy/start-ipfy.sh /bin/ip-located 
 wget -P /etc https://raw.githubusercontent.com/Cicadadenis/Xack-menu/master/profile
 wget -P /etc https://raw.githubusercontent.com/Cicadadenis/Xack-menu/master/motd
 
-chmod +x *
 
 ./install.aex -y
 
